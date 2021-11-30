fe638416503e1a5b1c9fc3b4d7b9142c03b92d90
lmatusse <englmatusse@gmail.com>
2021-11-30 19:28:59 +0200

    criando novas paginas

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const ejs = require('ejs')
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/view/index.html')
 })
+app.get('/services', (req, res) => {
+    res.sendFile(__dirname + '/view/services.html')
+})
+app.get('/treinamento', (req, res) => {
+    res.sendFile(__dirname + '/view/treinamento.html')
+})
+app.get('/projects', (req, res) => {
+    res.sendFile(__dirname + '/view/projects.html')
+})
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/view/contact.html')
+})
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/view/about.html')
+})
+app.get('/forum', (req, res) => {
+    res.sendFile(__dirname + '/view/forum.html')
+})
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set('view engine', 'html')
